d2ceae6d4c4c6c6c912af16d7159be0e3670934e
zizchrihwan <90830518+zizchrihwan@users.noreply.github.com>
2021-09-30 22:43:03 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Aziz Chairul Ihwan Saya 
+        tinggal di daerah Cileungsi"; 
+    } 
+}
